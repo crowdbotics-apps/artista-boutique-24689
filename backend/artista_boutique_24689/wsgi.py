f4cf042900071a5b62c531bd9cf15951d6e963ef
Crowdbotics <team@crowdbotics.com>
2021-02-24 03:35:29 +0000

    Update models

--- backend/artista_boutique_24689/wsgi.py
+++ backend/artista_boutique_24689/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'artista_boutique_24689.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "artista_boutique_24689.settings")
 
 application = get_wsgi_application()
