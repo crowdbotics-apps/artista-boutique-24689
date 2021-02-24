25d3b02d21a81b3571b7b0f62e4b84489a58ae16
Crowdbotics <team@crowdbotics.com>
2021-02-24 03:34:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/artista_boutique_24689/MainActivity.java
@@ -0,0 +1,15 @@
+package com.artista_boutique_24689;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "artista_boutique_24689";
+    }
+}
